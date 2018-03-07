ecb26274d0c6d398d806364c70973c5bf089ba1a
Aoiryuu <19joroka@waipahuh.k12.hi.us>
2018-03-07 13:59:31 -1000

    Done all i can remember

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ var array = ["Coke","Pepsi","Mountain Dew", "Root Beer"]
 var beverage = "sprite"
 array.pop()
 array.push(beverage)
-console.log(array)
+
+
+
 for (var i = array.length - 1; i >= 0; i--) {
 	array[i]
 }
+
+console.log(array)
